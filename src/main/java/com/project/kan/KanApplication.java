62df75f7cf96e4ab91c6a969ef130f7f4ec66340
kiran c chand <kirancchand@gmail.com>
2022-04-06 12:38:13 +0530

    commit for master

---
+++ src/main/java/com/project/kan/KanApplication.java
@@ -0,0 +1,13 @@
+package com.project.kan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/kan/KanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.kan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
