9978a0594795b65ab402185e93831bab827f039e
kiran c chand <kirancchand@gmail.com>
2022-04-06 22:11:39 +0530

    push with changes

--- src/main/java/com/project/kan/security/controller/HelloController.java
+++ src/main/java/com/project/kan/security/controller/HelloController.java
@@ -1,5 +1,13 @@
 package com.project.kan.security.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+	@RequestMapping({ "/hello" })
+	public String firstPage() {
+		return "Hello World";
+	}
 
 }
