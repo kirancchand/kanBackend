ded1bd07780dd29f0aec3ff247c876d21d0b214d
kiran c chand <kirancchand@gmail.com>
2022-04-07 11:28:47 +0530

    push with authorisation username

--- src/main/java/com/project/kan/security/controller/HelloController.java
+++ src/main/java/com/project/kan/security/controller/HelloController.java
@@ -1,13 +1,23 @@
 package com.project.kan.security.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.kan.security.config.JwtTokenUtil;
+
 @RestController
 public class HelloController {
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+	
 	@RequestMapping({ "/hello" })
-	public String firstPage() {
-		return "Hello World";
+	public String firstPage(@RequestHeader (name="Authorization") String token) {
+		
+
+		String myUserName=jwtTokenUtil.getUsernameFromToken(token.substring(7));
+		return myUserName;
 	}
 
 }
